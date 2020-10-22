a5f3d25e2f158b77ea8911947fc2b4e812cfcb1c
ethan <ethanbarr64@gmail.com>
2020-10-22 13:06:57 +1000

    edit app.py to have F and C conversion

--- app.py
+++ app.py
@@ -14,5 +14,18 @@ def greet(name=""):
     return "Hello {}".format(name)
 
 
+@app.route('/F/<celsius>')
+def celsius_to_fahrenheit(celsius=""):
+    fahrenheit = int(celsius) * 9.0 / 5 + 32
+    return "{} in Celsius gives {} in Fahrenheit".format(celsius, fahrenheit)
+
+
+@app.route('/C/<fahrenheit>')
+def fahrenheit_to_celsius(fahrenheit=""):
+    celsius = 5 / 9 * (int(fahrenheit) - 32)
+    return "{} in Fahrenheit gives {} in celsius".format(fahrenheit, celsius)
+
+
+
 if __name__ == '__main__':
     app.run()
